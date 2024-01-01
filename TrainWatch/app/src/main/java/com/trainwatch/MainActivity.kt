d96729cd116c0f42d29ebe271c5f9fdf0795b2de
Jeff Asamoah <ja3180@nyu.edu>
2024-01-01 14:55:54 -0500

    Side tab

--- TrainWatch/app/src/main/java/com/trainwatch/MainActivity.kt
+++ TrainWatch/app/src/main/java/com/trainwatch/MainActivity.kt
@@ -2,11 +2,15 @@ package com.trainwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.GestureDetector
 import android.view.MenuItem
+import android.view.MotionEvent
 import android.view.SubMenu
 import android.view.View
 import android.widget.Button
 import androidx.core.os.bundleOf
+import androidx.core.view.GestureDetectorCompat
 import androidx.core.view.GravityCompat
 import androidx.core.view.children
 import androidx.drawerlayout.widget.DrawerLayout
@@ -19,15 +23,17 @@ import com.trainwatch.enums.Line
 
 class MainActivity : AppCompatActivity() {
 
-    private var city: City? = City.NYC
-    private var drawerLayout: DrawerLayout? = null
-    private var navView: NavigationView? = null
+    private var city: City = City.NYC
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
+    private lateinit var cityGestureDetector: GestureDetectorCompat
     private var selectedRoutes: MutableSet<String> = refreshRoutes(city)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         drawerLayout = this.findViewById(R.id.drawer_layout)
+        cityGestureDetector = GestureDetectorCompat(this, CityGestureListener())
         navView = this.findViewById(R.id.route_navigation_drawer)
 
         if(savedInstanceState == null){
@@ -43,14 +49,14 @@ class MainActivity : AppCompatActivity() {
                 override fun onDrawerOpened(drawerView: View) {
                     val navView = drawerView as NavigationView
                     val cityView = navView.menu.children.first{child -> child.itemId == R.id.city_item}
-                    cityView.subMenu?.children?.forEach { it.isChecked = it.title == city?.name }
+                    cityView.subMenu?.children?.forEach { it.isChecked = it.title == city.name }
                     val legendView = navView.menu.children.first{child -> child.itemId == R.id.route_item}
                     legendView.subMenu?.children?.forEach { it.isChecked = it.title in selectedRoutes }
                     super.onDrawerOpened(drawerView)
                 }
             }
-            drawerLayout?.addDrawerListener(drawerListener)
-            navView?.setNavigationItemSelectedListener {i ->
+            drawerLayout.addDrawerListener(drawerListener)
+            navView.setNavigationItemSelectedListener {i ->
                 val newCheckVal = !i.isChecked
                 i.isChecked = newCheckVal
                 if(newCheckVal){
@@ -60,60 +66,70 @@ class MainActivity : AppCompatActivity() {
                 }
                 false// Return false to have display reflect the current list
             }
-
-            val t: Button = findViewById(R.id.test_button)
-            t.setOnClickListener { drawerLayout?.openDrawer(GravityCompat.END) }
         }
     }
 
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        Log.i(Constants.TRAIN_TAG, "Touch event ${event.toString()}")
+        cityGestureDetector.onTouchEvent(event!!)
+        return super.onTouchEvent(event)
+    }
     fun onCityMenuItemClick(item: MenuItem){
         val cityName: String = item.title.toString()
         if(cityName in City.cityNames){
             this.city = City.valueOf(cityName)
         }
-        val cityView = navView?.menu?.children?.first{child -> child.itemId == R.id.city_item}
-        cityView?.subMenu?.children?.forEach { it.isChecked = it.title == cityName }
+        val cityView = navView.menu.children.first{ child -> child.itemId == R.id.city_item}
+        cityView.subMenu?.children?.forEach { it.isChecked = it.title == cityName }
         this.selectedRoutes = this.refreshRoutes(this.city)
         this.createLegend()
     }
 
     private fun createLegend(){
-        city?.let {c ->
-            val lines = Line.cityLineNames[c.name]
-            val legendView = navView!!.menu.children.first{child -> child.itemId == R.id.route_item}
-            val routeMenu: SubMenu? = legendView.subMenu
-            routeMenu?.let { menu ->
-                menu.clear()
-                lines?.forEach { lineName ->
-                    val lineNameData: List<String> = lineName.split("_")
-                    val reducedLineName = lineNameData.slice(1 until lineNameData.size).joinToString("-")
-                    val menuItem = menu.add(reducedLineName)
-                    menuItem.isCheckable = true
-                    menuItem.isChecked = selectedRoutes.contains(reducedLineName)
-                }
+        val lines = Line.cityLineNames[city.name]
+        val legendView = navView.menu.children.first{child -> child.itemId == R.id.route_item}
+        val routeMenu: SubMenu? = legendView.subMenu
+        routeMenu?.let { menu ->
+            menu.clear()
+            lines?.forEach { lineName ->
+                val lineNameData: List<String> = lineName.split("_")
+                val reducedLineName = lineNameData.slice(1 until lineNameData.size).joinToString("-")
+                val menuItem = menu.add(reducedLineName)
+                menuItem.isCheckable = true
+                menuItem.isChecked = selectedRoutes.contains(reducedLineName)
             }
         }
     }
 
     private fun createSubwayFragment(){
-        city?.let {c ->
-            val bundle = bundleOf(
-                Pair(Constants.CITY_KEY, c.name),
-                Pair(Constants.ROUTES_KEY, selectedRoutes.toList())
-            )
-            supportFragmentManager.commit {
-                replace<SubwayFragment>(R.id.subway_fragment_container, args=bundle)
-                setReorderingAllowed(true)
-                addToBackStack(c.name)
-            }
+        val bundle = bundleOf(
+            Pair(Constants.CITY_KEY, city.name),
+            Pair(Constants.ROUTES_KEY, selectedRoutes.toList())
+        )
+        supportFragmentManager.commit {
+            replace<SubwayFragment>(R.id.subway_fragment_container, args=bundle)
+            setReorderingAllowed(true)
+            addToBackStack(city.name)
         }
     }
 
-    private fun refreshRoutes(city: City?): MutableSet<String>{
-        return Line.cityLineNames[city?.name]!!.map {l ->
+    private fun refreshRoutes(city: City): MutableSet<String>{
+        return Line.cityLineNames[city.name]!!.map {l ->
             val lineNameData: List<String> = l.split("_")
             val res = lineNameData.slice(1 until lineNameData.size).joinToString("-")
             res
         }.toMutableSet()
+    }
+
+    private class CityGestureListener: GestureDetector.SimpleOnGestureListener(){
+        override fun onScroll(
+            e1: MotionEvent?,
+            e2: MotionEvent,
+            distanceX: Float,
+            distanceY: Float
+        ): Boolean {
+            Log.i(Constants.TRAIN_TAG, "Scroll event!!")
+            return super.onScroll(e1, e2, distanceX, distanceY)
+        }
     }
 }
